f91432584930a9e90d618111c74dff590222516d
kdihalas <kdihalas@gmail.com>
2022-12-27 23:59:25 +0000

    install vbox

---
+++ modules/virtualisation/virtualbox.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}:
+
+{
+  virtualisation = {
+    virtualbox = {
+      host = {
+        enable = true;
+        enableExtensionPack = true;
+      };
+      guest = {
+        enable = true;
+        x11 = true;
+      };
+    };
+  };
+
+  users.extraGroups.vboxusers.members = [ "kostas" ]
+}
